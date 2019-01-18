5e69af8a3fdd37e85f8f765d74fefe2b96aa416f
nhung1121998 <nguyenthinhung1121998@gmail.com>
2019-01-18 08:25:47 +0700

    giaodich
    giaodich

---
+++ ClientApp/views/giaodich/banhang/index.ts
@@ -0,0 +1,11 @@
+import { component } from '@app/common/ko';
+@component({
+    url: '/giaodich/banhang',
+    title: '#giaodich_banhang',
+    styles: require('./style.scss'),
+    template: require('./index.html'),
+    resources: require('./resources.json')
+})
+export class GiaoDichBanHangViewModel {
+
+}
--- ClientApp/views/giaodich/index.ts
+++ ClientApp/views/giaodich/index.ts
@@ -3,3 +3,4 @@ import '@views/giaodich/hoadon';
 import '@views/giaodich/nhaphang';
 import '@views/giaodich/trahangnhap';
 import '@views/giaodich/xuathuy';
+import '@views/giaodich/banhang';
