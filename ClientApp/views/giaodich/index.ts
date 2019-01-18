434537b509ff32629a2575a12a6850cf79489482
nhung1121998 <nguyenthinhung1121998@gmail.com>
2019-01-18 11:43:30 +0700

    tra
    form trả hàng

--- ClientApp/views/giaodich/index.ts
+++ ClientApp/views/giaodich/index.ts
@@ -1,6 +1,7 @@
-import '@views/giaodich/loadgiaodich';
+import '@views/giaodich/trahang';
 import '@views/giaodich/hoadon';
 import '@views/giaodich/nhaphang';
 import '@views/giaodich/trahangnhap';
 import '@views/giaodich/xuathuy';
 import '@views/giaodich/banhang';
+import '@views/giaodich/modal/modaltrahang';
---
+++ ClientApp/views/giaodich/modal/modaltrahang/index.ts
@@ -0,0 +1,12 @@
+import { component } from '@app/common/ko';
+
+@component({
+    title: 'giaodich_modaltrahang',
+    name: 'modaltrahang',
+    styles: require('./style.scss'),
+    template: require('./index.html'),
+    resources: require('./resources.json')
+})
+export class GiaoDichModalTraHangViewModel {
+
+}
